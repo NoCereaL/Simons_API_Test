509dd90a28e140035f8f97d77570201da67c1635
NoCereaL <therealskm@hotmail.com>
2022-06-15 09:54:30 -0400

    Update

---
+++ index.js
@@ -0,0 +1,51 @@
+﻿
+const express = require('express');
+const app = express();
+const PORT = 8103;
+
+app.use(express.json())
+
+app.listen(
+    PORT, () => console.log('its live on http://localhost:${PORT}')
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '😀',
+        size: 'large'
+    })
+
+});
+
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a Logo'})
+    }
+
+    res.send({
+        tshirt: '😀 with your ${logo} and ID of ${id}',
+    });
+});
+
+
+/*
+const app = require('express')();
+const PORT = 8102;
+
+
+app.listen(
+    PORT, () => console.log('Its live on http://localhost:${PORT}')
+)
+
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'shirt',
+        size: 'medium'
+    })
+});
+*/
